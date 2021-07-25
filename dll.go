d19de75c543e30c81eb50518255831058969628a
Owen Kirby <oskirby@gmail.com>
2021-07-25 12:36:21 -0700

    Initial branch with wireguard tunnel.dll project.

---
+++ dll.go
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2019-2021 WireGuard LLC. All Rights Reserved.
+ */
+
+package main
+
+import (
+	"C"
+
+	"golang.org/x/crypto/curve25519"
+	"golang.org/x/sys/windows"
+
+	"golang.zx2c4.com/wireguard/windows/conf"
+	"github.com/mozilla-mobile/wireguard-tunnel-dll/tunnel"
+
+	"crypto/rand"
+	"log"
+	"path/filepath"
+	"unsafe"
+)
+
+//export WireGuardTunnelService
+func WireGuardTunnelService(confFile16 *uint16) bool {
+	confFile := windows.UTF16PtrToString(confFile16)
+	conf.PresetRootDirectory(filepath.Dir(confFile))
+	tunnel.UseFixedGUIDInsteadOfDeterministic = true
+	err := tunnel.Run(confFile)
+	if err != nil {
+		log.Printf("Service run error: %v", err)
+	}
+	return err == nil
+}
+
+//export WireGuardGenerateKeypair
+func WireGuardGenerateKeypair(publicKey *byte, privateKey *byte) {
+	publicKeyArray := (*[32]byte)(unsafe.Pointer(publicKey))
+	privateKeyArray := (*[32]byte)(unsafe.Pointer(privateKey))
+	n, err := rand.Read(privateKeyArray[:])
+	if err != nil || n != len(privateKeyArray) {
+		panic("Unable to generate random bytes")
+	}
+	privateKeyArray[0] &= 248
+	privateKeyArray[31] = (privateKeyArray[31] & 127) | 64
+
+	curve25519.ScalarBaseMult(publicKeyArray, privateKeyArray)
+}
+
+func main() {}
--- go.mod
+++ go.mod
@@ -1,9 +1,14 @@
-module golang.zx2c4.com/wireguard
+module github.com/mozilla-mobile/wireguard-tunnel-dll
 
 go 1.16
 
 require (
-	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
-	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22
+	golang.org/x/text v0.3.7-0.20210524175448-3115f89c4b99
+	golang.zx2c4.com/wireguard v0.0.0-20210624150102-15b24b6179e0
+	golang.zx2c4.com/wireguard/windows v0.3.16
 )
+
+replace golang.zx2c4.com/wireguard => ./
